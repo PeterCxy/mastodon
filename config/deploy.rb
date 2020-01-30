1d6e36c84f75bbb5e8c3a5ce093ad82da80cf08c
Peter Cai <peter@typeblog.net>
2020-01-30 11:29:48 +0800

    update default repo url to mine

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 
 lock '3.12.1'
 
-set :repo_url, ENV.fetch('REPO', 'https://github.com/tootsuite/mastodon.git')
+set :repo_url, ENV.fetch('REPO', 'https://github.com/PeterCxy/mastodon.git')
 set :branch, ENV.fetch('BRANCH', 'master')
 
 set :application, 'mastodon'
